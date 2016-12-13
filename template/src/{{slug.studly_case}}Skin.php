0bbb3e791e366fa63205b043cd7c3288a2413b9e
Adnan <adnan@websemantics.ca>
2016-12-13 09:58:30 -0500

    Update

--- template/src/{{slug.studly_case}}Skin.php
+++ template/src/{{slug.studly_case}}Skin.php
@@ -5,7 +5,7 @@ use Anomaly\Streams\Platform\Addon\Addon;
 /**
  * Class {{slug|studly_case}}Skin
  *
- {{docblock}}
+{{docblock|raw}}
  * @package   {{vendor|studly_case}}\{{slug|studly_case}}Skin
  */
 
